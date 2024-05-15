aa49ebea19e71e32144bbf46d78c58150e8df93e
Maria <msinukova@gmail.com>
2024-05-16 00:17:23 +0300

    Add Customer info reading and displaying

---
+++ src/app/Customer.java
@@ -0,0 +1,33 @@
+package app;
+
+public class Customer {
+
+    // Виправте декларацію змінних класу
+    private String name;
+    private String phone;
+
+    // Виправте визначення конструктора
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    // Пропишіть нижче getters для всіх змінних класу
+
+}
---
+++ src/app/Main.java
@@ -0,0 +1,31 @@
+package app;
+
+import java.util.Scanner;
+
+// Виправте цей клас
+public class Main {
+
+    public static void main(String[] args) {
+        Customer customer = getCustomer(getData());
+        String output = "Customer: " + customer.getName() +
+                ", phone: " + customer.getPhone();
+        getOutput(output);
+    }
+
+    public static String[] getData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter customer name: ");
+        String custName = sc.nextLine().trim();
+        System.out.print("Enter customer phone: ");
+        String custPhone = sc.nextLine().trim();
+        return new String[]{custName, custPhone};
+    }
+
+    public static Customer getCustomer(String[] data) {
+        return new Customer(data[0], data[1]);
+    }
+
+    public static void getOutput(String output) {
+        System.out.println(output);
+    }
+}
